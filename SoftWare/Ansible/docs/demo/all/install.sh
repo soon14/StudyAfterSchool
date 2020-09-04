efd1d1fa14166b99c3e232af85f0f74df386c412
root <root@localhost.localdomain>
2020-09-04 08:57:02 +0000

    1

--- SoftWare/Ansible/docs/demo/all/install.sh
+++ SoftWare/Ansible/docs/demo/all/install.sh
@@ -2,7 +2,7 @@
  # @Author: wjn
  # @Date: 2020-09-04 01:15:31
  # @LastEditors: wjn
- # @LastEditTime: 2020-09-04 08:00:47
+ # @LastEditTime: 2020-09-04 08:42:57
 ### 
 #!/bin/bash
 YUM_SOURCE_DIR=/etc/yum.repos.d
@@ -28,4 +28,4 @@ function install_ansible() {
 }
 
 
-ansible-playbook playbook.yml -i host.yml --tags="nginx"
+ansible-playbook playbook.yml -i host.yml --tags="redis"
