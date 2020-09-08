225094ee0ff711300636dd8041e5540b1d923be4
wjn0918 <43337754+wjn0918@users.noreply.github.com>
2020-09-08 09:16:30 +0800

    1

--- SoftWare/Ansible/docs/demo/face_recognition/install.sh
+++ SoftWare/Ansible/docs/demo/face_recognition/install.sh
@@ -2,10 +2,11 @@
  # @Author: wjn
  # @Date: 2020-09-07 08:16:06
  # @LastEditors: wjn
- # @LastEditTime: 2020-09-07 09:17:04
+ # @LastEditTime: 2020-09-08 01:15:15
 ### 
 #!/bin/bash
 
+# ansible-playbook playbook.yml -i host.yml --tags="python"
 
 # ansible-playbook playbook.yml -i host.yml --tags="cmake"
 
@@ -14,8 +15,8 @@
 # ansible-playbook playbook.yml -i host.yml --tags="dlib"
 
 # 安装依赖
-python3 -m pip install Click-7.0-py2.py3-none-any.whl face_recognition_models-0.3.0-py2.py3-none-any.whl numpy-1.16.2-cp36-cp36m-manylinux1_x86_64.whl Pillow-5.4.1-cp36-cp36m-manylinux1_x86_64.whl
+# ansible-playbook playbook.yml -i host.yml --tags="dep" 
 
 # ansible-playbook playbook.yml -i host.yml --tags="click"
 
-# ansible-playbook playbook.yml -i host.yml --tags="face_recognition"
+ansible-playbook playbook.yml -i host.yml --tags="face_recognition"
