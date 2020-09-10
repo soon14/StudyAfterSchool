b1347930e2b8dfb0471ac067aff4d369c6c89247
王江南 <wjn_0918@163.com>
2020-09-10 09:33:51 +0000

    1

--- SoftWare/jenkins/doc/playbooks/install.sh
+++ SoftWare/jenkins/doc/playbooks/install.sh
@@ -2,7 +2,7 @@
  # @Author: wjn
  # @Date: 2020-09-10 09:01:56
  # @LastEditors: wjn
- # @LastEditTime: 2020-09-10 09:02:21
+ # @LastEditTime: 2020-09-10 09:31:12
 ### 
 #!/bin/bash
 # 需要先安装curl
@@ -11,5 +11,7 @@ yum install -y --nogpgcheck curl
 # 运行ansible,失败重试，多运行几次
 ansible-galaxy install geerlingguy.jenkins
 ansible-playbook playbook.yml
+
+
 
 
---
+++ SoftWare/jenkins/doc/playbooks/setting.md
@@ -0,0 +1,8 @@
+<!--
+ * @Author: wjn
+ * @Date: 2020-09-10 09:31:43
+ * @LastEditors: wjn
+ * @LastEditTime: 2020-09-10 09:33:14
+-->
+# [安装前配置](参考.md)
+
